f9da05f903b6c042a01a93c9fcac0853e3c559d6
Tuan Dinh <dinhanhtuan191@gmail.com>
2024-10-23 11:42:17 +0700

    Remove file 5-Extern - Static ...

--- 5- Extern - Static - Volatile - Register/Extern/third_file.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int b = 23;
-
-int add(int a, int b)
-{
-    return (a+b);
-}
--- 5- Extern - Static - Volatile - Register/Static/second_file.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FILE1_H
-#define _FILE1_H
-
-static int a;
-int b;
-
-static void showfile();
-void display();
-#endif
