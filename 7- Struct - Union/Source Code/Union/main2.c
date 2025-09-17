71fb4bbe510a159b850b123b5b2b75e03ec7fc04
Tuan <dinhanhtuan191@gmail.com>
2025-09-17 17:28:34 +0700

    Update file main2.c trong Union

--- 7- Struct - Union/Source Code/Union/main2.c
+++ 7- Struct - Union/Source Code/Union/main2.c
@@ -12,11 +12,18 @@ union Data data;
 
 int main()
 {
-    printf("Size of frame: %d\n",sizeof(data));
+    printf("Size of frame: %zd\n",sizeof(data));
                             //0x03      0x02    0x01     0x00                            
     data.var2 = 4294967200; //11111111 11111111 11111111 10100000
 
     printf("var1 = %d\n",data.var1); //160 - 0b10100000
     printf("var2 = %u\n",data.var2); //4294967200 - 0b11111111111111111111111110100000
     printf("var3 = %d\n",data.var3); //65440 - 0b11111111 10100000
+    
+    data.var3 = 600; //0b 00000010 01011000
+    printf("var1 = %d\n",data.var1); //88 - 0b01011000
+    printf("var2 = %u\n",data.var2); //4294902360 - 0b11111111 11111111 00000010 01011000
+    printf("var3 = %d\n",data.var3); //600 - 0b00000010 01011000
+    
+    
 }
